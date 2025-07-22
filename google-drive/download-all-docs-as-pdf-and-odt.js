7833a62c8ed2b2f76bc2ba109e8883609521fbe4
fullstackleo777 <leo@fullstackleo.net>
2025-07-22 06:04:29 -0500

    add download all as pdf and odt files script

---
+++ google-drive/download-all-docs-as-pdf-and-odt.js
@@ -0,0 +1,27 @@
+function generateDownloadLinks() {
+  const folderId = Browser.inputBox('Enter the Folder ID containing the Google Docs:');
+  const folder = DriveApp.getFolderById(folderId);
+  const files = folder.getFilesByType(MimeType.GOOGLE_DOCS);
+  let output = '';
+
+  while (files.hasNext()) {
+    const file = files.next();
+    const fileId = file.getId();
+    const fileName = file.getName();
+    
+    const pdfLink = `https://docs.google.com/document/d/${fileId}/export?format=pdf`;
+    const odtLink = `https://docs.google.com/document/d/${fileId}/export?format=odt`;
+
+    output += `${fileName}\nPDF: ${pdfLink}\nODT: ${odtLink}\n\n`;
+    Logger.log(`${fileName}:\nPDF: ${pdfLink}\nODT: ${odtLink}`);
+  }
+
+  if (output) {
+    const htmlOutput = HtmlService.createHtmlOutput(`<pre>${output}</pre>`)
+      .setWidth(800)
+      .setHeight(600);
+    SpreadsheetApp.getUi().showModalDialog(htmlOutput, 'Download Links');
+  } else {
+    SpreadsheetApp.getUi().alert('No Google Docs found in the specified folder.');
+  }
+}
